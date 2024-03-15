2fa5bff413d6e7fe5ea5e8541ce3b41ecc37fa61
SidAsif <asif050md@gmail.com>
2024-03-15 14:26:13 +0530

    responsive

--- src/Pages/CountryDetails.js
+++ src/Pages/CountryDetails.js
@@ -6,6 +6,7 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import "./Country.css";
+import { Paper, Stack } from "@mui/material";
 
 export default function CountryDetails(props) {
   const { countryCode } = useParams();
@@ -19,59 +20,65 @@ export default function CountryDetails(props) {
   console.log("CountryCode: ", countryCode);
 
   return (
-    <div>
-      <h1>COUNTRY DETAILS</h1>
-      <Card className="countryDetailsWrapper  " sx={{ maxWidth: 800, mt: 4 }}>
-        <CardMedia sx={{ mb: 2, mt: 2 }}>
-          <img src={detail.flags?.png} alt={detail.name} />
-        </CardMedia>
-        <CardContent style={{ marginLeft: "20px" }}>
-          <Typography
-            gutterBottom
-            variant="h5"
-            component="div"
-            style={{ background: "lightGrey" }}
-          >
-            Name : {detail.name}
-          </Typography>
-          <Typography gutterBottom variant="h5" component="div">
-            Capital : {detail.capital}
-          </Typography>
-          <Typography
-            gutterBottom
-            variant="h5"
-            style={{ background: "lightGrey" }}
-          >
-            Region : {detail.region}
-          </Typography>
-          <Typography gutterBottom variant="h5">
-            Area : {detail.area} km<sup>2</sup>
-          </Typography>
-          <Typography
-            gutterBottom
-            variant="h5"
-            style={{ background: "lightGrey" }}
-          >
-            Population : {detail.population}
-          </Typography>
-          <Typography gutterBottom variant="h5">
-            {" "}
-            Currencies :{" "}
-            {detail.currencies?.map((currency) => currency.name).join(",")}{" "}
-          </Typography>
-          <Typography
-            gutterBottom
-            variant="h5"
-            style={{ background: "lightGrey" }}
-          >
-            Languages :{" "}
-            {detail.languages?.map((language) => language.name).join(",")}{" "}
-          </Typography>
-          <Typography gutterBottom variant="h5">
-            Timezone : {detail.timezones}
-          </Typography>
-        </CardContent>
-      </Card>
-    </div>
+    <Stack justifyContent={"center"} alignItems={"center"}>
+      <Typography variant="h4" mt={7}>
+        COUNTRY DETAILS
+      </Typography>
+      <Paper elevation={6} sx={{ mt: 2 }}>
+        <Card className="countryDetailsWrapper  " sx={{ maxWidth: 800, mt: 4 }}>
+          <CardMedia sx={{ m: 2 }}>
+            <img src={detail.flags?.png} alt={detail.name} />
+          </CardMedia>
+          <CardContent style={{ marginLeft: "20px" }}>
+            <Typography
+              gutterBottom
+              variant="h5"
+              component="div"
+              style={{ background: "lightGrey" }}
+            >
+              Name : {detail.name}
+            </Typography>
+            <Typography gutterBottom variant="h5" component="div">
+              Capital : {detail.capital}
+            </Typography>
+            <Typography
+              gutterBottom
+              variant="h5"
+              style={{ background: "lightGrey" }}
+            >
+              Region : {detail.region}
+            </Typography>
+            <Typography gutterBottom variant="h5">
+              Area : {detail.area} km<sup>2</sup>
+            </Typography>
+            <Typography
+              gutterBottom
+              variant="h5"
+              style={{ background: "lightGrey" }}
+            >
+              Population : {detail.population}
+            </Typography>
+            <Typography gutterBottom variant="h5">
+              {" "}
+              Currencies :{" "}
+              {detail.currencies
+                ?.map((currency) => currency.name)
+                .join(",")}{" "}
+            </Typography>
+            <Typography
+              gutterBottom
+              variant="h5"
+              style={{ background: "lightGrey" }}
+            >
+              Languages :{" "}
+              {detail.languages?.map((language) => language.name).join(",")}{" "}
+            </Typography>
+            <Typography gutterBottom variant="h5">
+              Timezone : {detail.timezones}
+            </Typography>
+          </CardContent>
+        </Card>
+      </Paper>
+    </Stack>
   );
 }
